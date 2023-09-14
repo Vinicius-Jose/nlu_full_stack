81261a1bb9fe6c7abe68a3e357ec6cd3862e12ba
Vinícius José Pierri Nogueira <vinijosenog@hotmail.com>
2023-09-14 12:54:41 -0300

    Adicionado models ao adming

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,14 +6,24 @@ from .models import CarMake, CarModel
 
 # Register your models here.
 
+
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ["car_make", "name", "dealer_id", "car_type", "year"]
 
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ["name", "description"]
+    inlines = [CarModelInline]
+
 
 # Register models here
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
